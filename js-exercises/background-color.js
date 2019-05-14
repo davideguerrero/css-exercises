28d82b722906c73c8cb457490b3691f3c3871c05
davideguerrero <davidencaladaguerrero@gmail.com>
2019-05-15 00:24:12 +0200

    new_repository

--- js-exercises/background-color.js
+++
@@ -1,8 +0,0 @@
-console.log('It is me');
-var selectElement = document.getElementById('selectElement')
-selectElement.addEventListener('change', selectElementChange)
-
-function selectElementChange() {
-  // console.log(selectElement.value);
-  document.body.style.backgroundColor = selectElement.value;
-};
